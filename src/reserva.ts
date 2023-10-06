93d33242869bb1258896a90bc06a7331edd8be72
roxootiago <tiago.roxo@hotmail.com>
2023-10-06 15:23:36 -0300

    adicionando reserva de quartos

--- src/reserva.ts
+++ src/reserva.ts
@@ -1,35 +1,42 @@
 class Reserva {
-    private dataIda:number
-    private quantDias:number
-    private quantPessoas:number
-    
-    constructor(dataIda:number, quantDias:number, quantPessoas:number){
-        this.dataIda = dataIda;
-        this.quantDias = quantDias;
-        this.quantPessoas = quantPessoas;
-    }
-    
-// getters
-getDataIda(){
+  private dataIda: number;
+  private quantDias: number;
+  private quantPessoas: number;
+  private tipoQuarto: string;
+
+  constructor(
+    dataIda: number,
+    quantDias: number,
+    quantPessoas: number,
+    tipoQuarto: string
+  ) {
+    this.dataIda = dataIda;
+    this.quantDias = quantDias;
+    this.quantPessoas = quantPessoas;
+    this.tipoQuarto = tipoQuarto;
+  }
+
+  // getters
+  getDataIda() {
     return this.dataIda;
-}
-getQuantDias(){
+  }
+  getQuantDias() {
     return this.quantDias;
-}
-getQuantPessoas(){
+  }
+  getQuantPessoas() {
     return this.quantPessoas;
+  }
+
+  // setters
+  public setDataIda(value: number) {
+    this.dataIda = value;
+  }
+  public setQuantDias(value: number) {
+    this.dataIda = value;
+  }
+  public setQuantPessoas(value: number) {
+    this.quantPessoas = value;
+  }
 }
 
-// setters
-public setDataIda(value:number){
-    this.dataIda = value
-}
-public setQuantDias(value:number){
-    this.dataIda = value
-}
-public setQuantPessoas(value:number){
-    this.quantPessoas = value
-}
- }
-
-    
+export default Reserva;
